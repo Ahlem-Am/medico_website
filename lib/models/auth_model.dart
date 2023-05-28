673f917fd3b6b5c55fe1323696d5dfc22d879037
wissaltesting <wissaltesting@gmail.com>
2023-05-28 23:58:35 +0200

    commit

--- lib/models/auth_model.dart
+++ lib/models/auth_model.dart
@@ -10,6 +10,7 @@ class AuthModel extends ChangeNotifier {
   List<Map<String, dynamic>> favDoc = []; //get latest favorite doctor
   List<dynamic> _fav = []; //get all fav doctor id in list
 
+
   bool get isLogin {
     return _isLogin;
   }
